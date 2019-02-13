4a84d706376e8ac1a6270cbfd74350a4b89f9063
Xiaobai0419 <minexiaobai@hotmail.com>
2019-02-13 19:36:41 +0800

    调整为引用公共maven包形式的sdk,行业Bean转移到公共sdk,删除冗余包、类，测试通过.

--- src/main/java/com/sunfield/microframe/common/utils/DateUtils.java
+++
@@ -1,8 +0,0 @@
-package com.sunfield.microframe.common.utils;
-
-public class DateUtils {
-
-    public final static String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";//已改为正确格式，格式错误会造成转换出的时间串错误！
-
-    public final static String DEFAULT_TIME_ZONE = "GMT+8";
-}
--- src/main/java/com/sunfield/microframe/common/utils/UUID.java
+++
@@ -1,9 +0,0 @@
-package com.sunfield.microframe.common.utils;
-
-public class UUID {
-
-	public static String getUUID(){
-		return java.util.UUID.randomUUID().toString().replace("-", "");
-	}
-	
-}
